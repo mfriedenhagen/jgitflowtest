b9702cbe85babf8d2c0329f31a49d81d910028dd
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2015-11-11 22:04:38 +0100

    Init

---
+++ src/main/java/de/friedenhagen/jgitflowtest/App.java
@@ -0,0 +1,13 @@
+package de.friedenhagen.jgitflowtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
